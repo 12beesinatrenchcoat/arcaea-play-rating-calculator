016a94387e14134c0d7365f5f3f1ddb11545ddc4
Andy Chan / 12beesinatrenchcoat <12beesinatrenchcoat+git@protonmail.com>
2022-07-16 20:45:57 -0400

    attempt 1 at fixing github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = "production" === "development";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: adapter(),
 		prerender: {
 			default: true
 		},
-		adapter: adapter()
+		paths: {
+			base: dev ? "" : "/arcaea-potential-calculator",
+		},
 	}
 };
 
