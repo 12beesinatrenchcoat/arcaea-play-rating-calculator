b8a23362a633df324492c437e3b2bfde79e8a21a
12beesinatrenchcoat / Andy Chan <12beesinatrenchcoat+git@protonmail.com>
2022-09-21 11:30:03 -0400

    update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
 
-const dev = "production" === "development";
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
-		prerender: {
-			default: true
-		},
 		paths: {
-			base: dev ? "" : "/arcaea-play-rating-calculator",
+			base: "/arcaea-play-rating-calculator",
 		},
 	},
 	preprocess: preprocess({})
